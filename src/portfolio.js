2b11a97057bf0fc9fffd11c9b48f21c639b90e74
Osama-you <osama.w.younis@gmail.com>
2021-11-26 19:32:37 +0200

    Update portfolio data

--- src/portfolio.js
+++ src/portfolio.js
@@ -3,4 +3,22 @@ const header = {
   title: 'JS.',
 };
 
-export { header };
+const projects = [];
+
+const skills = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Material UI',
+  'Redux',
+  'SASS',
+  'Git',
+];
+
+const contact = {
+  email: 'osama.w.younis@gmail.com',
+};
+
+export { header, contact, projects, skills };
